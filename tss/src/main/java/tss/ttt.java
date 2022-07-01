8d91ded14f8be15730fe1baa176c5ea0e44886b5
Nigel <nigeltjk1@gmail.com>
2022-07-01 15:25:18 +0800

    Tesing it out

---
+++ tss/src/main/java/tss/ttt.java
@@ -0,0 +1,9 @@
+package tss;
+
+public class ttt {
+
+    public static void main(String[] args) {
+        System.out.println("fihbfs");
+    }
+    
+}
---
+++ tsss/src/main/java/tsss/fssf.java
@@ -0,0 +1,9 @@
+package tsss;
+
+public class fssf {
+
+    public static void main(String[] args) {
+        System.out.println("fssffs");
+    }
+    
+}
